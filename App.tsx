86a4e9c8d8728e472e132d7a6826f784ed71d6e1
Thales Aguiar <dev.thalesaguiar@gmail.com>
2023-12-24 23:20:50 -0300

    feat: instaled tamagui

--- App.tsx
+++ App.tsx
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { Text, View, StatusBar } from "react-native";
+import { TamaguiProvider } from "@tamagui/core";
+import config from "./tamagui.config";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TamaguiProvider config={config}>
+      {
+        <View
+          style={{
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "#202024",
+          }}
+        >
+          <StatusBar
+            barStyle="light-content"
+            backgroundColor="transparent"
+            translucent
+          />
+          {fontsLoaded ? (
+            <Text style={{ fontFamily: "Roboto_700Bold", color: "#fff" }}>
+              Teste
+            </Text>
+          ) : (
+            <View />
+          )}
+        </View>
+      }
+    </TamaguiProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
