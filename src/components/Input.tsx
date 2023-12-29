4fe61c97f46d058580743081341bb4e177213ffe
Thales Aguiar <dev.thalesaguiar@gmail.com>
2023-12-28 23:20:04 -0300

    feat: created new component

---
+++ src/components/Input.tsx
@@ -0,0 +1,15 @@
+import { Input as TamaguiInput } from "tamagui";
+
+export function Input() {
+  return (
+    <TamaguiInput
+      backgroundColor={"#121214"}
+      height={50}
+      padding={10}
+      borderWidth={0}
+      color={"#fff"}
+      fontSize={22}
+      marginBottom={20}
+    />
+  );
+}
